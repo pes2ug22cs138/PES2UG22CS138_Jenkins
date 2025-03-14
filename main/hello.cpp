1b6b19498c79692387c040575999b48a742ae0a2
pes2ug22cs138 <pes2ug22cs138@pesu.pes.edu>
2025-03-14 16:25:18 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Lab_8\n";
+  cout << "Lab8\n";
   return 0;
 }
